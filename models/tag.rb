23837b5914b92195b1c9a117a54e3d53310d44c5
Callum <callumpb@hotmail.com>
2017-10-22 17:47:11 +0100

    set up initial tag.rb

---
+++ models/tag.rb
@@ -0,0 +1,11 @@
+require_relative('../db/sql_runner.rb')
+
+class Tag
+
+  attr_reader: :id, :name
+
+  def initialize(options)
+    @id = options['id'].to_i
+    @name = options['name']
+  end
+end
