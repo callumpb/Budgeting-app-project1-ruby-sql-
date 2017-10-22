fad828dfc63fd41bf87817d3af303078bea23f8c
Callum <callumpb@hotmail.com>
2017-10-22 17:48:47 +0100

    set up initial transactions.rb

---
+++ models/transactions.rb
@@ -0,0 +1,13 @@
+require_relative('../db/sql_runner.rb')
+
+class Merchant
+
+  attr_reader: :id, :amount, :merchant_id, :tag_id
+
+  def initialize(options)
+    @id = options['id'].to_i
+    @amount = options['amount'].to_i
+    @merchant_id = options['merchant_id'].to_i
+    @tag_id = options['tag_id'].to_i
+  end
+end
