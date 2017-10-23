426fd69a46bc4dd75fc34cd566b2059b75bbc1ce
Callum <callumpb@hotmail.com>
2017-10-23 22:56:52 +0100

    added transactions.save

--- models/transactions.rb
+++ models/transactions.rb
@@ -10,4 +10,16 @@ class Transaction
     @merchant_id = options['merchant_id'].to_i
     @tag_id = options['tag_id'].to_i
   end
+
+  def save
+    sql = "INSERT INTO transactions
+    (amount, merchant_id, tag_id)
+    VALUES ($1, $2, $3)
+    RETURNING id"
+    values = [@amount, @merchant_id, @tag_id]
+    result = SqlRunner.run(sql, values)
+    @id = result.first['id'].to_i
+  end
+
+  
 end
