10dccdee3bb22e59f93ce29f417076cbaab1fc1c
Callum <callumpb@hotmail.com>
2017-10-23 22:26:34 +0100

    added tag.save method

--- models/tag.rb
+++ models/tag.rb
@@ -8,4 +8,14 @@ class Tag
     @id = options['id'].to_i
     @name = options['name']
   end
+
+  def save
+    sql = "INSERT INTO tags (name)
+    VALUES ($1)
+    RETURNING id"
+    values = [@name]
+    result = SqlRunner.new(sql, values)
+    @id = result.first['id'].to_i
+  end
+  
 end
