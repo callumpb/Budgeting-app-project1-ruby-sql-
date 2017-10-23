03fb533e7f270d1d12671fb20bb50964f3f88359
Callum <callumpb@hotmail.com>
2017-10-23 22:39:45 +0100

    added tag.delete_all method + minor syntax edits

--- models/tag.rb
+++ models/tag.rb
@@ -14,17 +14,21 @@ class Tag
     VALUES ($1)
     RETURNING id"
     values = [@name]
-    result = SqlRunner.new(sql, values)
+    result = SqlRunner.run(sql, values)
     @id = result.first['id'].to_i
   end
 
   def self.all
     sql = "SELECT * FROM tags"
     values = []
-    result = SqlRunner.new(sql, values)
+    result = SqlRunner.run(sql, values)
     result.map { |tag| Tag.new(tag) }
     return result
   end
 
-  
+  def delete_all
+    sql = "DELETE * FROM tags"
+    values = []
+    result = SqlRunner.run(sql, values)
+  end
 end
