65456aab28730a28e0e8adf091f693a37e4bcfef
Callum <callumpb@hotmail.com>
2017-10-24 12:58:36 +0100

    updated transactions_controller require_relatives

--- controllers/transactions_controller.rb
+++ controllers/transactions_controller.rb
@@ -2,6 +2,8 @@ require('sinatra')
 require('sinatra/contrib/all')
 require('pry')
 require_relative('../models/transaction.rb')
+require_relative('../models/tag.rb')
+require_relative('../models/merchant.rb')
 
 get '/transactions' do
   @transactions = Transaction.all
